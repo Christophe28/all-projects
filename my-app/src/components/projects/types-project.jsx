578989fa476b955a492a18c9e1a2e97efc484ad5
christophe <christophebuffe@gmail.com>
2022-06-29 11:14:54 +0200

    picture link

--- my-app/src/components/projects/types-project.jsx
+++ my-app/src/components/projects/types-project.jsx
@@ -1,31 +1,45 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import naturePicture from "../../assets/nature-chill.png";
+import trainPicture from "../../assets/train-chill.png";
+import townDestroy from "../../assets/ville-en-ruine.jpg";
+
 const TypesProjects = ({ myEvent }) => {
+    
     return (
         <div className="container-projects">
             <section className="big-project">
-                <NavLink
-                to="/big-projects"
-                >
-                    Gros projets
-                </NavLink>
+                <div className="layer">
+                    <NavLink
+                    to="/big-projects"
+                    >
+                        <img src={naturePicture} alt="Panorama nature" />
+                    </NavLink>
+                </div>
+                <p>Gros projets</p>
             </section>
 
             <section className="soft-project">
-                <NavLink
-                to="/soft-projects"
-                >
-                    Petit projets
-                </NavLink>
+                <div className="layer">
+                    <NavLink
+                    to="/soft-projects"
+                    >
+                        <img src={townDestroy} alt="Artwork ganja white night" />
+                    </NavLink>
+                </div>
+                <p>Petits projets perso</p>
             </section>
 
             <section className="on-working">
-                <NavLink
-                to="/on-working"
-                >
-                    Projets en cours
-                </NavLink>
+                <div className="layer">
+                    <NavLink
+                    to="/on-working"
+                    >
+                        <img src={trainPicture} alt="Fille qui attend que le train passe" className="picture-to-rework-dimension"/>
+                    </NavLink>
+                </div>
+                <p>Projets en cours</p>
             </section>
         </div>
     );

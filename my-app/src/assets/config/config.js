20aaf4597081d63751ac0cde4ef7bf4883faa783
christophe <christophebuffe@gmail.com>
2022-08-26 18:04:40 +0200

    test url problem onlin project

---
+++ my-app/src/assets/config/config.js
@@ -0,0 +1,14 @@
+export const projects = [
+    {
+        name: "Configurateur Inseetu",
+        type: "bigProject",
+        picture: "",
+        url: ""
+    },
+    {
+        name: "Randomiser Pokémon",
+        type: "softProject",
+        picture: "",
+        url: ""
+    }
+]
--- my-app/src/pages/home.jsx
+++ my-app/src/pages/home.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import TypesProjects from '../components/projects/types-project';
 import ViewWrapper from '../components/view-wrapper/view-wrapper';
 
+import { projects } from '../assets/config/config';
+
 const Home = () => {
     return (
         <div className="container-home">
             <ViewWrapper>
                 <h1>Développeur Web</h1>
-                <h1>Deploiement</h1>
-                <TypesProjects/>       
+                <TypesProjects/>   
             </ViewWrapper>
         </div>
     );

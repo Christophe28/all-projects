d2b780b2ba9174b5df0ed4dd71e9a3ae196b5150
christophe <christophebuffe@gmail.com>
2022-09-05 16:32:39 +0200

    swiper test

--- my-app/src/pages/All-projects.jsx
+++ my-app/src/pages/All-projects.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper ,SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 import BreadcrumbTrail from '../components/view-wrapper/breadcrumb-trail';
 import ViewWrapper from '../components/view-wrapper/view-wrapper';
@@ -6,23 +12,53 @@ import ViewWrapper from '../components/view-wrapper/view-wrapper';
 import { projects } from '../config/config';
 
 const AllProjects = () => {
+
     return (
         <div>
             <ViewWrapper>
                 <BreadcrumbTrail>
-                    <section className="container-my-projects">
-                        <h1 style={{textAlign: "center"}}>Mes projets</h1>
+                    <section className="container-my-projects" >
+                        <h1 style={{textAlign: "center"}}>PortFolio</h1>
                         {
                             projects.map((project) => (
-                                <section>
-                                    <h3>{project.name}</h3>
-                                    <img src={project.picture} alt="my-projects" />
-                                    <a href={project.url}>
-                                        <p>{project.description}</p>
-                                    </a>
+                                <section className="img-content" key={project.name}>
+                                    <div className="img-container hover">
+                                        <span>
+                                            <h3>{project.name}</h3>
+                                            <a href={project.url}>
+                                                <p>{project.description}</p>
+                                            </a>
+                                        </span>
+                                        <img src={project.picture} alt="my-projects" />    
+                                    </div>
+                                    <div className="view-project">
+                                        <a href={project.url}>Voir le site</a>
+                                    </div>
                                 </section>
                             ))
                         }
+                        <Swiper
+                            modules={[Navigation, Pagination, Scrollbar, A11y]}
+                            spaceBetween={50}
+                            slidesPerView={3}
+                            navigation
+                            pagination={{clickable: true}}
+                            scrollbar={{draggable: true}}
+                            onSwiper={(swiper) => console.log(swiper)}
+                            onSlideChange={() => console.log("slide change")}
+                        >
+                            <div className="swiper">
+                                <div className="swiper-wrapper">
+                                    <div className="swiper-slide">Div1</div>
+                                    <div className="swiper-slide">Div2</div>
+                                    <div className="swiper-slide">Div3</div>
+                                </div>
+                                <div className="swiper-pagination"></div>
+                                <div className="swiper-button-prev"></div>
+                                <div className="swiper-button-next"></div>
+                                <div className="swiper-scrollbar"></div>
+                            </div>
+                        </Swiper>
                     </section>
                 </BreadcrumbTrail>
             </ViewWrapper>
